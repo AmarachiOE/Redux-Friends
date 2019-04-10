e50fa2123bbe4c94fa395e99cf5d2ff8ce2cddd7
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-10 16:06:17 -0700

    Set up basic reducer, createdStore with rootReducer, applyMiddleware, thunk, and logger;, and wrapped App in Provider.

---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,12 @@
+import { /* import ACTION_TYPES*/ } from "../actions";
+
+const initialState = {
+    friends: []
+};
+
+export const friendsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
