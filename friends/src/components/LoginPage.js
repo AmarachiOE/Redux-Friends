15fefe9c8c0ae8908389cb6200441f94f0ed5808
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-11 13:43:09 -0700

    Built action creator for login and updated LoginPage component with event handler functions for input field and onSubmit for form. Need to update reducer.

--- friends/src/components/LoginPage.js
+++ friends/src/components/LoginPage.js
@@ -1,24 +1,66 @@
 import React from "react";
 import { connect } from "react-redux";
+import { login } from "../actions";
 
-// class component because it will have login form
 
-const LoginPage = props => {
+class LoginPage extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  // for input fields
+  handleChanges = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  // for form onSubmit
+  // if post req for login is successful, this .then will also be invoked
+  // if login is successful, then navigate to url/protected page
+
+  submitForm = e => {
+    e.preventDefault();
+    this.props.login(this.state.credentials).then(() => {
+      console.log("SUCCESS");
+      this.props.history.push("/friends-list"); 
+    });
+  }
+
+
+  render() {
     return (
-        <div >
-          <form className="Login-Form" onSubmit={props.login}>
-            <input 
-            onChange={props.handleChanges}
-            placeholder="Username" />
-            <input 
-            onChange={props.handleChanges}placeholder="Password" />
-            <button className="login-button">Login</button>
-          </form>
-        </div>
-      );
+      <div >
+        <form className="Login-Form" onSubmit={this.submitForm}>
+          <input 
+          type="string"
+          name="username"
+          value={this.state.credentials.username}
+          onChange={this.handleChanges}
+          placeholder="Username" 
+          />
+          <input 
+          type="string"
+          name="password"
+          value={this.state.credentials.password}
+          onChange={this.handleChanges}
+          placeholder="Password" 
+          />
+          <button className="login-button">Login</button>
+        </form>
+      </div>
+    );
+  }  
+  
 }
 
-export default LoginPage;
+export default connect(null, { login })(LoginPage);
 
 /*
 
